292cb3aab5b13d186b77747d6f3bce215ae8584d
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-08-17 20:50:45 +0200

    build: remove comments on build

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from "rollup-plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
 import resolve from "rollup-plugin-node-resolve";
 import url from "rollup-plugin-url";
+import cleanup from "rollup-plugin-cleanup";
 
 import pkg from "./package.json";
 
@@ -28,5 +29,6 @@ export default {
     }),
     resolve(),
     commonjs(),
+    cleanup(),
   ],
 };
