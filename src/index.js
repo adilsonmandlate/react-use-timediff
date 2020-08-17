82d758030db63a6cafdefe2f19fe7ecb746056e8
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-08-17 20:50:11 +0200

    feat: return negative values to past dates

--- src/index.js
+++ src/index.js
@@ -23,10 +23,6 @@ export const useTimeDiff = (eventDate, options = {}) => {
    * Extract date details given a date difference
    */
   const calculateDifference = (difference) => {
-    if (difference < 0) {
-      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
-    }
-
     return {
       days: Math.floor(difference / (1000 * 60 * 60 * 24)),
       hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
