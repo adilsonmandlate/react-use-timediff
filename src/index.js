1ef79511dd8e119aa5dfac17107f6e61a0b929b5
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-09-10 01:17:24 +0200

    feat: calculate the number of remaining years and months

--- src/index.js
+++ src/index.js
@@ -10,9 +10,11 @@ import { useState, useEffect } from "react";
  * @returns {{days: Number, hours: Number, minutes: Number, seconds: Number}} Time difference
  */
 export const useTimeDiff = (eventDate, options = {}) => {
-  const { startDate = Date.now(), interval = 1000, live = false } = options;
+  const finalDate = new Date(eventDate);
+  const { interval = 1000, live = false } = options;
 
   const [time, setTime] = useState({
+    years: 0,
     days: 0,
     hours: 0,
     minutes: 0,
@@ -20,26 +22,98 @@ export const useTimeDiff = (eventDate, options = {}) => {
   });
 
   /**
-   * Extract date details given a date difference
+   * Determine whether a year is leap year.
+   * @param {number} year
+   * @returns {boolean}
    */
-  const calculateDifference = (difference) => {
+  const isLeapYear = (year) => {
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400;
+  };
+
+  /**
+   * Extract date details given a date difference
+   * @param {Date} initialDate Date to calculate differences
+   * @returns {object}
+   */
+  const calculateDifference = (initialDate) => {
+    const initialYear = initialDate.getFullYear();
+    const finalYear = finalDate.getFullYear();
+
+    let yearDiff = finalYear - initialYear;
+    let monthDiff = finalDate.getMonth() - initialDate.getMonth();
+    let dayDiff = finalDate.getDate() - initialDate.getDate();
+
+    let hoursDiff = finalDate.getHours() - initialDate.getHours();
+    let minutesDiff = finalDate.getMinutes() - initialDate.getMinutes();
+    let secondsDiff = finalDate.getSeconds() - initialDate.getSeconds();
+
+    const febNumberDays = isLeapYear(initialDate.getFullYear()) ? 29 : 28;
+    const daysOfMonths = [
+      31,
+      febNumberDays,
+      31,
+      30,
+      31,
+      30,
+      31,
+      31,
+      30,
+      31,
+      30,
+      31,
+    ];
+
+    if (finalDate.getTime() < initialDate.getTime()) {
+      return new Error(
+        "Cannot make the calculations. The start date is greater than the final date"
+      );
+    }
+
+    if (secondsDiff < 0) {
+      secondsDiff += 60;
+      minutesDiff = minutesDiff - 1;
+    }
+
+    if (minutesDiff < 0) {
+      minutesDiff += 60;
+      hoursDiff = hoursDiff - 1;
+    }
+
+    if (hoursDiff < 0) {
+      hoursDiff += 24;
+      dayDiff = dayDiff - 1;
+    }
+
+    if (monthDiff < 0) {
+      yearDiff = yearDiff - 1;
+      monthDiff += 12;
+    }
+
+    if (dayDiff < 0) {
+      monthDiff = monthDiff - 1;
+      dayDiff += daysOfMonths[initialDate.getMonth()];
+    }
+
     return {
-      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-      minutes: Math.floor((difference / 1000 / 60) % 60),
-      seconds: Math.floor((difference / 1000) % 60),
+      years: yearDiff,
+      months: monthDiff,
+      days: dayDiff,
+      hours: hoursDiff,
+      minutes: minutesDiff,
+      seconds: secondsDiff,
     };
   };
 
   useEffect(() => {
     let timer = null;
-    let differenceTime = eventDate - startDate;
-    setTime(calculateDifference(differenceTime));
+    const initialDate = options.startDate
+      ? new Date(options.startDate)
+      : new Date();
+    setTime(calculateDifference(initialDate));
 
     if (live) {
       timer = setInterval(() => {
-        differenceTime = eventDate - Date.now();
-        setTime(calculateDifference(differenceTime));
+        setTime(calculateDifference(new Date()));
       }, interval);
     }
 
